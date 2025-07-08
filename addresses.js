15e53da9839368c2122444792d4beb4525834130
FreedomPros <christineeremija@solarpros.io>
2025-07-08 15:14:32 -0700

    Update addresses.js

--- addresses.js
+++ addresses.js
@@ -17,7 +17,7 @@ window.addresses = [
   {
     name: "MOMENTVS (70-80) CA",
     state: "California",
-    address: "10532 Acacia St. Rancho Cucomongo, Ca. 91730",
+    address: "10532 Acacia St. Rancho Cucamonga, CA. 91730",
     coords: [34.0921, -117.5737],
     contacts: [
       {
@@ -33,7 +33,7 @@ window.addresses = [
   {
     name: "DAUNTLESS & MOTION (60-70) CA",
     state: "California",
-    address: "FURY OFFICE: 15440 Laguna Canyon Rd. Suite 210. Irvine, CA. 92618",
+    address: "15440 Laguna Canyon Rd. Suite 210. Irvine, CA. 92618",
     coords: [33.6644, -117.7576],
     contacts: [
       {
@@ -52,160 +52,5 @@ window.addresses = [
     ],
     date: "June 30 - July 1"
   },
-  {
-    name: "BAN, UNITED, & FORGE (65-75) CA",
-    state: "California",
-    address: "10532 Acacia St. Rancho Cucomongo, Ca. 91730",
-    coords: [34.0921, -117.5743],
-    contacts: [
-      {
-        team: "BAN",
-        people: [
-          { name: "Grant Misbach", phone: "(801) 643-9206" }
-        ]
-      },
-      {
-        team: "UNITED",
-        people: [
-          { name: "Casey Kendall", phone: "(206) 554-1489" },
-          { name: "Cameron Kendall", phone: "(206) 661-4244" }
-        ]
-      },
-      {
-        team: "FORGE",
-        people: [
-          { name: "Logan Berger", phone: "(208) 954-1233" }
-        ]
-      }
-    ],
-    date: "July 3 - 4"
-  },
-  {
-    name: "THE SPARTANS, EMPIRE & Kyros LA (50+) CA",
-    state: "California",
-    address: "27141 Hideaway Ave. Santa Clarita, 91351",
-    coords: [34.4264, -118.4842],
-    contacts: [
-      {
-        team: "THE SPARTANS",
-        people: [
-          { name: "Spencer Deavila", phone: "(661) 416-7979" },
-          { name: "Zak Hattab", phone: "(925) 435-7328" }
-        ]
-      },
-      {
-        team: "EMPIRE",
-        people: [
-          { name: "Sebastian Rodriguez", phone: "(813) 679-0944" }
-        ]
-      }
-    ],
-    date: "July 7 - 8"
-  },
-  {
-    name: "PRIME & APOLLO (30) CA",
-    state: "California",
-    address: "FreedomForever: 3010 San Antonio Dr. Fowler, CA 93625",
-    coords: [36.6177, -119.6782],
-    contacts: [
-      {
-        team: "PRIME",
-        people: [
-          { name: "Hayden Young", phone: "385-219-0979" }
-        ]
-      },
-      {
-        team: "APOLLO",
-        people: [
-          { name: "Nate Girsberger", phone: "(385) 210-7846" }
-        ]
-      }
-    ],
-    date: "July 10 - 11"
-  },
-  {
-    name: "MOTION (50+) CA",
-    state: "California",
-    address: "6144 Industrial Way. Suite B. Livermore, CA. 94551",
-    coords: [37.7038, -121.7680],
-    contacts: [
-      {
-        team: "MOTION",
-        people: [
-          { name: "Junior Bandeira", phone: "773-501-5285" }
-        ]
-      }
-    ],
-    date: "July 14 - 15"
-  },
-  {
-    name: "EMPIRE, VITALITY, ENVISION, EXTREME & SLINGERS (140) IL",
-    state: "Illinois",
-    address: "4300 Commerce Ct, unit 240 Lisle, IL 60532",
-    coords: [41.8023, -88.0884],
-    contacts: [
-      {
-        team: "EMPIRE",
-        people: [
-          { name: "Rocky Niger", phone: "(727) 748-6089" }
-        ]
-      },
-      {
-        team: "VITALITY",
-        people: [
-          { name: "Chayse Kidd", phone: "(208) 360-3024" }
-        ]
-      },
-      {
-        team: "ENVISION",
-        people: [
-          { name: "Drew Sanders", phone: "(918) 706-5054" }
-        ]
-      },
-      {
-        team: "EXTREME",
-        people: [
-          { name: "Ben Pappas", phone: "(717) 925-6924" },
-          { name: "Tyler Whimpey", phone: "(801) 696-8581" }
-        ]
-      },
-      {
-        team: "SLINGERS",
-        people: [
-          { name: "Crew Smithson", phone: "(385) 401-9266" },
-          { name: "Jared Monhey", phone: "(801) 631-1493" }
-        ]
-      }
-    ],
-    date: "July 17, 18, 19"
-  },
-  {
-    name: "SOL, RELENTLESS & BOUKNIGHT (40-45) MI",
-    state: "Michigan",
-    address: "33043 Hamilton Ct, Farmington Hills, MI 48334",
-    coords: [42.5322, -83.3746],
-    contacts: [
-      {
-        team: "SOL",
-        people: [
-          { name: "Korey Lopez", phone: "(517) 914-4057" }
-        ]
-      },
-      {
-        team: "RELENTLESS",
-        people: [
-          { name: "Nick Fischer", phone: "(831) 251-5612" },
-          { name: "Adam Martinez", phone: "(480) 862-5118" }
-        ]
-      },
-      {
-        team: "BOUKNIGHT",
-        people: [
-          { name: "Matt Lent", phone: "(954) 243-1732" }
-        ]
-      }
-    ],
-    date: "July 22 - 23"
-  },
-  // ...continue for all other events, ensuring you add the "state" property...
+  // Add all other entries here, following the same structure and including a "state" property for each.
 ];
