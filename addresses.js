67a875f7034e16cd715d3c366bf5438e264930aa
FreedomPros <christineeremija@solarpros.io>
2025-07-09 14:37:57 -0700

    Update addresses.js

--- addresses.js
+++ addresses.js
@@ -1,221 +1,86 @@
 window.addresses = [
+  // June sample events
   {
-    name: "FURY (150 - 200) CA",
+    name: "FURY (200) CA",
     state: "California",
-    participants: "150-200",
+    participants: "200",
     address: "15440 Laguna Canyon Rd. Suite 210. Irvine, CA. 92618",
     coords: [33.6644, -117.7576],
     contacts: [
-      {
-        team: "FURY",
-        people: [
-          { name: "Jay Merrifield", phone: "801-336-6924" }
-        ]
-      }
+      { team: "FURY", participants: 200, people: [ { name: "Jay Merrifield", phone: "801-336-6924" } ] }
     ],
     date: "June 26 - 27"
   },
   {
-    name: "MOMENTVS (70-80) CA",
+    name: "DAUNTLESS, MOTION (130) CA",
     state: "California",
-    participants: "70-80",
-    address: "10532 Acacia St. Rancho Cucomongo, Ca. 91730",
-    coords: [34.0921, -117.5737],
-    contacts: [
-      {
-        team: "MOMENTVS",
-        people: [
-          { name: "Cole Sexton", phone: "320-339-1204" },
-          { name: "Christian Harrison", phone: "(678) 702-7185" }
-        ]
-      }
-    ],
-    date: "June 23 - 24"
-  },
-  {
-    name: "DAUNTLESS & MOTION (60-70) CA",
-    state: "California",
-    participants: "60-70",
-    address: "FURY OFFICE: 15440 Laguna Canyon Rd. Suite 210. Irvine, CA. 92618",
+    participants: "130",
+    address: "15440 Laguna Canyon Rd. Suite 210. Irvine, CA. 92618",
     coords: [33.6644, -117.7576],
     contacts: [
-      {
-        team: "DAUNTLESS",
-        people: [
-          { name: "Casey Mosier", phone: "801-787-2621" }
-        ]
-      },
-      {
-        team: "MOTION",
-        people: [
-          { name: "Jacob Garza", phone: "(623) 308-9737" },
-          { name: "Tilo Chen", phone: "(510) 402-9699" }
-        ]
-      }
+      { team: "DAUNTLESS", participants: 70, people: [ { name: "Casey Mosier", phone: "801-787-2621" } ] },
+      { team: "MOTION", participants: 60, people: [ { name: "Jacob Garza", phone: "(623) 308-9737" } ] }
     ],
     date: "June 30 - July 1"
   },
+  // July sample events (from your photo)
   {
-    name: "BAN, UNITED, & FORGE (65-75) CA",
+    name: "Prime & Apollo (CA) (40)",
     state: "California",
-    participants: "65-75",
-    address: "10532 Acacia St. Rancho Cucomongo, Ca. 91730",
-    coords: [34.0921, -117.5743],
+    participants: "40",
+    address: "Prime & Apollo CA Address",
     contacts: [
-      {
-        team: "BAN",
-        people: [
-          { name: "Grant Misbach", phone: "(801) 643-9206" }
-        ]
-      },
-      {
-        team: "UNITED",
-        people: [
-          { name: "Casey Kendall", phone: "(206) 554-1489" },
-          { name: "Cameron Kendall", phone: "(206) 661-4244" }
-        ]
-      },
-      {
-        team: "FORGE",
-        people: [
-          { name: "Logan Berger", phone: "(208) 954-1233" }
-        ]
-      }
-    ],
-    date: "July 3 - 4"
-  },
-  {
-    name: "THE SPARTANS, EMPIRE & Kyros LA (50+) CA",
-    state: "California",
-    participants: "50+",
-    address: "27141 Hideaway Ave. Santa Clarita, 91351",
-    coords: [34.4264, -118.4842],
-    contacts: [
-      {
-        team: "THE SPARTANS",
-        people: [
-          { name: "Spencer Deavila", phone: "(661) 416-7979" },
-          { name: "Zak Hattab", phone: "(925) 435-7328" }
-        ]
-      },
-      {
-        team: "EMPIRE",
-        people: [
-          { name: "Sebastian Rodriguez", phone: "(813) 679-0944" }
-        ]
-      }
-    ],
-    date: "July 7 - 8"
-  },
-  {
-    name: "PRIME & APOLLO (30) CA",
-    state: "California",
-    participants: "30",
-    address: "FreedomForever: 3010 San Antonio Dr. Fowler, CA 93625",
-    coords: [36.6177, -119.6782],
-    contacts: [
-      {
-        team: "PRIME",
-        people: [
-          { name: "Hayden Young", phone: "385-219-0979" }
-        ]
-      },
-      {
-        team: "APOLLO",
-        people: [
-          { name: "Nate Girsberger", phone: "(385) 210-7846" }
-        ]
-      }
+      { team: "Prime & Apollo", participants: 40, people: [] }
     ],
     date: "July 10 - 11"
   },
   {
-    name: "MOTION (50+) CA",
-    state: "California",
-    participants: "50+",
-    address: "6144 Industrial Way. Suite B. Livermore, CA. 94551",
-    coords: [37.7038, -121.7680],
+    name: "CHICAGO - ILLINOIS Empire/Vitality/Envision/Extreme/Slingers (119)",
+    state: "Illinois",
+    participants: "119",
+    address: "Chicago Event Address",
     contacts: [
-      {
-        team: "MOTION",
-        people: [
-          { name: "Junior Bandeira", phone: "773-501-5285" }
-        ]
-      }
+      { team: "Empire", participants: 25, people: [] },
+      { team: "Vitality", participants: 30, people: [] },
+      { team: "Envision", participants: 24, people: [] },
+      { team: "Extreme", participants: 22, people: [] },
+      { team: "Slingers", participants: 18, people: [] }
+    ],
+    date: "July 17 - 19"
+  },
+  {
+    name: "MOTION (60) CA",
+    state: "California",
+    participants: "60",
+    address: "MOTION CA Address",
+    contacts: [
+      { team: "MOTION", participants: 60, people: [] }
     ],
     date: "July 14 - 15"
   },
   {
-    name: "EMPIRE, VITALITY, ENVISION, EXTREME & SLINGERS (140) IL",
-    state: "Illinois",
-    participants: "140",
-    address: "4300 Commerce Ct, unit 240 Lisle, IL 60532",
-    coords: [41.8023, -88.0884],
-    contacts: [
-      {
-        team: "EMPIRE",
-        people: [
-          { name: "Rocky Niger", phone: "(727) 748-6089" }
-        ]
-      },
-      {
-        team: "VITALITY",
-        people: [
-          { name: "Chayse Kidd", phone: "(208) 360-3024" }
-        ]
-      },
-      {
-        team: "ENVISION",
-        people: [
-          { name: "Drew Sanders", phone: "(918) 706-5054" }
-        ]
-      },
-      {
-        team: "EXTREME",
-        people: [
-          { name: "Ben Pappas", phone: "(717) 925-6924" },
-          { name: "Tyler Whimpey", phone: "(801) 696-8581" }
-        ]
-      },
-      {
-        team: "SLINGERS",
-        people: [
-          { name: "Crew Smithson", phone: "(385) 401-9266" },
-          { name: "Jared Monhey", phone: "(801) 631-1493" }
-        ]
-      }
-    ],
-    date: "July 17, 18, 19"
-  },
-  {
-    name: "SOL, RELENTLESS & BOUKNIGHT (40-45) MI",
+    name: "MICHIGAN Sol, Relentless, Bouknight (53)",
     state: "Michigan",
-    participants: "40-45",
-    address: "33043 Hamilton Ct, Farmington Hills, MI 48334",
-    coords: [42.5322, -83.3746],
+    participants: "53",
+    address: "Michigan Event Address",
     contacts: [
-      {
-        team: "SOL",
-        people: [
-          { name: "Korey Lopez", phone: "(517) 914-4057" }
-        ]
-      },
-      {
-        team: "RELENTLESS",
-        people: [
-          { name: "Nick Fischer", phone: "(831) 251-5612" },
-          { name: "Adam Martinez", phone: "(480) 862-5118" }
-        ]
-      },
-      {
-        team: "BOUKNIGHT",
-        people: [
-          { name: "Matt Lent", phone: "(954) 243-1732" }
-        ]
-      }
+      { team: "Sol", participants: 20, people: [] },
+      { team: "Relentless", participants: 18, people: [] },
+      { team: "Bouknight", participants: 15, people: [] }
     ],
     date: "July 22 - 23"
   },
+  {
+    name: "TEXAS (50)",
+    state: "Texas",
+    participants: "50",
+    address: "Texas Event Address",
+    contacts: [
+      { team: "TEXAS", participants: 50, people: [] }
+    ],
+    date: "July 28 - 29"
+  },
+  // Your new July/Aug/August events:
   {
     name: "SOLAR MONEY GANG, OMNI, SPARTANS DFBUB, SUPREME (70) TX",
     state: "Texas",
@@ -223,32 +88,10 @@ window.addresses = [
     address: "3488 E Long Ave Fort Worth, TX 76137",
     coords: [32.8134, -97.3021],
     contacts: [
-      {
-        team: "SOLAR MONEY GANG",
-        people: [
-          { name: "Clayton Granch", phone: "(714) 612-2222" },
-          { name: "Josh Simmons", phone: "(682) 978-1939" }
-        ]
-      },
-      {
-        team: "OMNI",
-        people: [
-          { name: "Sam Parks", phone: "(714) 612-2222" },
-          { name: "Caleb Brown" }
-        ]
-      },
-      {
-        team: "SPARTANS DFBUB",
-        people: [
-          { name: "Pedro Kinlaw", phone: "(956) 340-6863" }
-        ]
-      },
-      {
-        team: "SUPREME",
-        people: [
-          { name: "Brandon Hoy", phone: "(541) 441-0779" }
-        ]
-      }
+      { team: "SOLAR MONEY GANG", people: [ { name: "Clayton Granch", phone: "(714) 612-2222" }, { name: "Josh Simmons", phone: "(682) 978-1939" } ] },
+      { team: "OMNI", people: [ { name: "Sam Parks", phone: "(714) 612-2222" }, { name: "Caleb Brown" } ] },
+      { team: "SPARTANS DFBUB", people: [ { name: "Pedro Kinlaw", phone: "(956) 340-6863" } ] },
+      { team: "SUPREME", people: [ { name: "Brandon Hoy", phone: "(541) 441-0779" } ] }
     ],
     date: "July 28 - 29"
   },
@@ -259,44 +102,25 @@ window.addresses = [
     address: "1920 Primewest Pkwy. #400. Katy, TX 77449",
     coords: [29.7841, -95.7317],
     contacts: [
-      {
-        team: "ELITE CHAKRA",
-        people: [
-          { name: "Clayton Granch", phone: "(714) 612-2222" }
-        ]
-      },
-      {
-        team: "IMPACT GROUP",
-        people: [
-          { name: "Andy Wang" }
-        ]
-      },
-      {
-        team: "THE MASTERS",
-        people: [
-          { name: "Layton Hix", phone: "(346) 503-6060" }
-        ]
-      },
-      {
-        team: "SPACE CITY",
-        people: [
-          { name: "Saint Termeteet", phone: "(713) 887-2739" }
-        ]
-      },
-      {
-        team: "THE STANDARD",
-        people: [
-          { name: "Hameed Hewaldi", phone: "(214) 934-1435" }
-        ]
-      },
-      {
-        team: "ELEVATE",
-        people: [
-          { name: "Matt Lent", phone: "(954) 243-1732" }
-        ]
-      }
+      { team: "ELITE CHAKRA", people: [ { name: "Clayton Granch", phone: "(714) 612-2222" } ] },
+      { team: "IMPACT GROUP", people: [ { name: "Andy Wang" } ] },
+      { team: "THE MASTERS", people: [ { name: "Layton Hix", phone: "(346) 503-6060" } ] },
+      { team: "SPACE CITY", people: [ { name: "Saint Termeteet", phone: "(713) 887-2739" } ] },
+      { team: "THE STANDARD", people: [ { name: "Hameed Hewaldi", phone: "(214) 934-1435" } ] },
+      { team: "ELEVATE", people: [ { name: "Matt Lent", phone: "(954) 243-1732" } ] }
     ],
     date: "July 31 - Aug 1"
   },
-  // ... and so on for the rest of your addresses ...
+  {
+    name: "ALL BAD (30-40) TX",
+    state: "Texas",
+    participants: "30-40",
+    address: "1900 E Howard Ln. Fluegerville, TX. 78660",
+    coords: [30.4196, -97.6305],
+    contacts: [
+      { team: "ALL BAD", people: [ { name: "Tyler Brown", phone: "(702) 815-5886" } ] }
+    ],
+    date: "Aug 4 - 5"
+  },
+  // Add further August/September events as needed!
 ];
